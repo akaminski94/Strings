4cd0c03f88f7bb3bf5570f7a954e43cb0a71ec11
Artur <akaminski94@outlook.com>
2018-07-18 13:32:08 +0200

    Aktualizacja klasy Word

--- src/akaminski/Word_class.java
+++ src/akaminski/Word_class.java
@@ -39,7 +39,7 @@ public class Word_class {
     }
 
     //Silnia uzywając rekurencji
-    public long fact (long n){
+    private long fact (long n){
 
         if (n <= 1)
             return 1; //if 0 || 1
@@ -49,20 +49,20 @@ public class Word_class {
     }
 
     //Dodawanie do list
-    public void Dodaj(String[]Tabela, List<String>list){
+    private void Dodaj(String[]Tabela, List<String>list){
         for(String x: Tabela)
             list.add(x);
     }
 
     //Wyswietl
-    public void Wyswietl(List<String>list){
+    private void Wyswietl(List<String>list){
         for(int i = 0; i<list.size(); i++){
             System.out.printf("%s ", list.get(i));
         }
     }
 
     //Edycja (przy uzyciu iteratora) list1, gdy list 2 zawiera takie same wartosci to usuwa je z list 1
-    public void Edytuj(List<String>list,List<String>list2){
+    private void Edytuj(List<String>list,List<String>list2){
         Iterator<String> it = list.iterator();
         while(it.hasNext()){
             if (list2.contains(it.next()))
@@ -71,50 +71,50 @@ public class Word_class {
     }
 
     //Dodawanie zawartości pomiędzy list'ami
-    public void DodajList(List<String>list,List<String>list2){
+    private void DodajList(List<String>list,List<String>list2){
         list.addAll(list2);
     }
 
     //Odwracanie
-    public void Reverse(List<String>list){
+    private void Reverse(List<String>list){
         ListIterator<String> rev = list.listIterator(list.size());
         while (rev.hasPrevious())
             System.out.printf("%s ", rev.previous());
     }
 
     //Usuwanie
-    public void Usun(List<String>list){
+    private void Usun(List<String>list){
         System.out.println("Który element?");
         int wybor = Integer.parseInt(odczyt.nextLine());
         list.remove(wybor);
     }
 
     //Konwersja List na Array
-    public void Con(List<String>list,String[]Tabela ){
+    private void Con(List<String>list,String[]Tabela ){
     Tabela = list.toArray(new String[list.size()]);
     }
 
     //Sortowanie Collections
-    public void Sort(List<String>list ){
+    private void Sort(List<String>list ){
         Collections.sort(list);//alfabetycznie
     }
     //Kopiowanie Collections
-    public void Cop(List<String>listdest,List<String>listsrc ){
+    private void Cop(List<String>listdest,List<String>listsrc ){
         Collections.copy(listdest,listsrc);
     }
 
     //Wypełnianie Collection
-    public void Fill(List<String>list ){
+    private void Fill(List<String>list ){
         Collections.fill(list, "X");
     }
 
     //Addall Collection
-    public void addall(List<String>srclist,String[]Tabela ){
+    private void addall(List<String>srclist,String[]Tabela ){
         Collections.addAll(srclist,Tabela);
     }
 
     //Sprawdzanie obu Collections
-    public void check(List<String>list,List<String>list2 ){
+    private void check(List<String>list,List<String>list2 ){
         boolean ch = Collections.disjoint(list,list2);
         if(ch){
             System.out.println("Nie mają nic wspólnego");
